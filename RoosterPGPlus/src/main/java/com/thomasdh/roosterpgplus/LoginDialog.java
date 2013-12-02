ee936af5c6cb868eab90d0ba0cdfd4c12977e3d6
Thomas den Hollander <denhollander.thomas@gmail.com>
2013-12-02 15:09:13 +0100

    LoginDialog jeva-bestand verwijderd

--- RoosterPGPlus/src/main/java/com/thomasdh/roosterpgplus/LoginDialog.java
+++
@@ -1,9 +0,0 @@
-package com.thomasdh.roosterpgplus;
-
-import android.app.Dialog;
-import android.content.Context;
-import android.view.LayoutInflater;
-
-/**
- * Created by Floris on 30-11-13.
- */
