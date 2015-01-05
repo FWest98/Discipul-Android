33d4d5e3b78c4b066dcf497157c6531de2106d08
Floris Westerman <floriswesterman@msn.com>
2015-01-05 10:19:33 +0100

    Fixed #211

--- RoosterPGPlus/src/main/java/com/thomasdh/roosterpgplus/Notifications/GCMIntentService.java
+++ RoosterPGPlus/src/main/java/com/thomasdh/roosterpgplus/Notifications/GCMIntentService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
@@ -54,6 +55,8 @@ public class GCMIntentService extends IntentService {
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setFullScreenIntent(contentPendingIntent, true)
                 .setContentIntent(contentPendingIntent)
+                .setLights(Color.RED, 300, 200)
+                .setVibrate(new long[] { 5000, 1000 })
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(message));
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
